2d9cfd5268fccac7e6a622efaa2ffd95b42bea44
janvg02 <jan.vangalen@student.pxl.be>
2023-12-08 15:45:36 +0100

    NavComponent af

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,28 @@ import FooterComponent from "@/components/FooterComponent.vue";
 
 <template>
 <HeaderComponent/>
+  <header class="start-header">
+    <div class="header-banner">
+      <h1 class="header-name">PhoneLab</h1>
+      <p class="sub-name">The phone reseller</p>
+      <h1 class="main-heading">Welcome</h1>
+      <h3 class="sub-heading">Need a new phone? We are the Apple reseller you are looking for!</h3>
+      <a href="overzicht.html"> <button class="header-button" >Bekijk ons assortiment</button></a>
+      <h2 class="product-main">Producten in de kijker</h2>
+      <a class="arrow-down"> <i class="fa-solid fa-angles-down fa-2xl"></i></a>
+    </div>
+  </header>
   <main class="start-main">
     <div class="product">
       <h3 class="product-price"><span class="price-sale">€1.299 </span><br>€999</h3>
-      <img src="src/assets/iphone-15-plus.webp" alt="15 plus" class="product-pro">
+      <img src="./assets/iphone-15-plus.webp" alt="15 plus" class="product-pro">
       <p class="product-name">Iphone 15 plus</p>
       <p class="product-name">Apple</p>
       <p class="product-name">256 GB</p>
     </div>
     <div class="product">
       <h3 class="product-price"><span class="price-sale">€999 </span><br>€749</h3>
-      <img src="src/assets/iphone-15.webp" alt="15" class="product-pro">
+      <img src="./assets/iphone-15.webp" alt="15" class="product-pro">
       <p class="product-name">Iphone 15</p>
       <p class="product-name">Apple</p>
       <p class="product-name">128 GB</p>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,44 +1,68 @@
-<script setup>
+<script>
+export default {
+  data() {
+    return {
 
+      navItems: [
+        {
+          path: "./index.html",
+          name:"Home"
+        },
+        {
+          path: "./overzicht.html",
+          name:"Products"
+        },
+        {
+          path: "/",
+          name:"Cart"
+        },
+        {
+          path: "/",
+          name:"Log in"
+        },
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <nav class="start-nav">
-    <ul class="nav-components">
-      <li><a href="#" class="logo"><i class="fa-solid fa-mobile fa-2xl"></i> </a></li>
-      <li><a href="#" class="home">Home</a></li>
-      <li><a href="overzicht.html">Assortiment</a></li>
-      <li><a href="login.html">Log in</a></li>
-      <li id="cart"><a href="winkelmand.html"><i class="fa-solid fa-cart-shopping fa-2xl"></i></a></li>
-    </ul>
+  <nav class="navbar">
+      <div class="navbar-navigation-right">
+        <ul class="nav-list">
+          <li v-for="navItem in navItems"
+              class="nav-list-item">
+            <a href="#" class="nav-list-link">{{ navItem.name }}</a>
+          </li>
+        </ul>
+      </div>
   </nav>
 </template>
 
-<style scoped>
-.start-nav .nav-components, .login-nav .nav-components, .overzicht-nav .nav-components, .detail-nav .nav-components {
-  display: flex;
-  flex-direction: row;
-  color: #f9F9F9;
-  list-style-type: none;
-  font-family: "Nunito", sans-serif;
-}
-.start-nav .nav-components li, .login-nav .nav-components li, .overzicht-nav .nav-components li, .detail-nav .nav-components li {
-  margin: 10px 30px;
-}
-.start-nav .nav-components li .logo, .login-nav .nav-components li .logo, .overzicht-nav .nav-components li .logo, .detail-nav .nav-components li .logo {
-  color: #3b83bd;
-}
-.start-nav .nav-components li a, .login-nav .nav-components li a, .overzicht-nav .nav-components li a, .detail-nav .nav-components li a {
-  text-decoration: none;
-  width: 20px;
-  height: auto;
-  padding: 10px;
-  color: #f9F9F9;
-}
-.start-nav .nav-components li a:hover, .login-nav .nav-components li a:hover, .overzicht-nav .nav-components li a:hover, .detail-nav .nav-components li a:hover {
-  color: #3b83bd;
-  background-color: #bbb;
-  border-radius: 2px;
-  transition: all 0.4s;
+<style>
+.navbar{
+  padding: 2%;
+  font-family: "Josefin Sans", sans-serif;
+  .nav-list{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    .nav-list-item{
+      list-style-type: none;
+      .nav-list-link{
+        color: #3b83bd;
+        text-decoration: none;
+        padding: 10px;
+
+      }
+      .nav-list-link:hover{
+        color: #2c3e50;
+        background-color: #3b83bd;
+        transition: all .4s;
+
+      }
+    }
+
+  }
 }
 </style>

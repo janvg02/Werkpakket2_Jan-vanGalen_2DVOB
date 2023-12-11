5445b18f8113778b90bd378c425bab846713c5ba
janvg02 <jan.vangalen@student.pxl.be>
2023-12-11 21:18:43 +0100

    producten toegevoegd

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -12,6 +12,7 @@ export default {
           <h1 class="item-overlay-titel">{{product.title}}</h1>
           <button class="item-overlay-button"><router-link to="/DetailView">Details</router-link></button>
           <p class="product-price">€{{product.price}} </p>
+          <p class="product-price">Color: {{product.color}} </p>
         </div>
         <img :src="product.imageLink" :alt="product.imageAlt" class="item-image">
       </div>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,12 +1,119 @@
 <script>
 import CardComponent from "@/components/CardComponent.vue";
+import {defineComponent} from "vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import NavComponent from "@/components/NavComponent.vue";
+import FooterComponent from "@/components/FooterComponent.vue";
+import FilterComponent from "@/components/FilterComponent.vue";
 
-export default {
-  components:{CardComponent},
-    props: {
-      products: Object
+export default defineComponent({
+  components: {CardComponent, HeaderComponent, NavComponent, FooterComponent,FilterComponent},
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          title: 'Iphone 15',
+          description:'Apple, 256gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Pink',
+          imageLink: 'src/assets/iphone-15.webp',
+          imageAlt: 'Iphone 15',
+          price: 749,
+          BTW: 749*0.21,
+          Stock: 5
+        },
+        {
+          id: 2,
+          title: 'Iphone 15 Plus',
+          description:'Apple, 128gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Black',
+          imageLink: 'src/assets/iphone-15-plus.webp',
+          imageAlt: 'Iphone 15 Plus',
+          price: 999,
+          BTW: 999*0.21,
+          Stock: 12
+        },
+        {
+          id: 3,
+          title: 'Iphone 14',
+          description:'Apple, 256gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Yellow',
+          imageLink: 'src/assets/iphone-14.webp',
+          imageAlt: 'Iphone 14',
+          price: 749,
+          BTW: 749*0.21,
+          Stock: 8
+        },
+        {
+          id: 4,
+          title: 'Iphone 12',
+          description:'Apple, 512gb, 48MP camera, Splash, water and dust resistant.',
+          imageLink: 'src/assets/iphone-12.webp',
+          color: 'White',
+          imageAlt: 'Iphone 12',
+          price: 649,
+          BTW: 649*0.21,
+          Stock: 3
+        },
+        {
+          id: 5,
+          title: 'Iphone 12 Mini',
+          description:'Apple, 256gb,48MP camera, Splash, water and dust resistant.',
+          color: 'Blue',
+          imageLink: 'src/assets/iphone-12-mini.webp',
+          imageAlt: 'Iphone 12 Mini',
+          price: 599,
+          BTW: 599*0.21,
+          Stock: 4
+        },
+        {
+          id: 6,
+          title: 'Iphone 13 Mini',
+          description:'Apple, 128gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Midnight',
+          imageLink: 'src/assets/iphone-13-mini.webp',
+          imageAlt: 'Iphone 13 mini',
+          price: 649,
+          BTW: 649*0.21,
+          Stock: 3
+        },
+        {
+          id: 7,
+          title: 'Iphone 15 Pro Max',
+          description:'Apple, 256gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Natural Titanium',
+          imageLink: 'src/assets/iphone-15-pro-max.webp',
+          imageAlt: 'Iphone 15 Pro Max',
+          price: 1199,
+          BTW: 1199*0.21,
+          Stock: 1
+        },
+        {
+          id: 8,
+          title: 'Iphone 15 Pro',
+          description:'Apple, 128gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Black Titanium',
+          imageLink: 'src/assets/iphone-15-pro.webp',
+          imageAlt: 'Iphone 15 Pro',
+          price: 999,
+          BTW: 999*0.21,
+          Stock: 3
+        },
+        {
+          id: 9,
+          title: 'Iphone 14 Plus',
+          description:'Apple, 512gb, 5G compatible, 48MP camera, Splash, water and dust resistant.',
+          color: 'Blue',
+          imageLink: 'src/assets/iphone-14-plus.png',
+          imageAlt: 'Iphone 14 Plus',
+          price: 979,
+          BTW: 979*0.21,
+          Stock: 5
+        },
+      ]
     }
-}
+  }
+})
 </script>
 
 <template>
@@ -48,10 +155,12 @@ export default {
       </div>
     </div>
   </div>
-  <CardComponent v-for="product in products"
-                 v-bind:key="product.id"
-                 v-bind:product="product"
-  />
+  <div class="gallery">
+    <CardComponent v-for="product in products"
+                   v-bind:key="product.id"
+                   v-bind:product="product"
+    />
+  </div>
 </template>
 
 <style scoped>

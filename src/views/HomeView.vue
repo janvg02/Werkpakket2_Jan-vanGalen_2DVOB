f65facc8f2f59dc99ed98082433c2e4600430728
janvg02 <jan.vangalen@student.pxl.be>
2023-12-12 21:54:30 +0100

    PopularProducts aangepast

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,61 +4,15 @@ import CardComponent from "@/components/CardComponent.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
 import NavComponent from "@/components/NavComponent.vue";
 import FooterComponent from "@/components/FooterComponent.vue";
+import PopularProductsComponent from "@/components/PopularProductsComponent.vue";
 export default defineComponent({
-  components: {CardComponent, HeaderComponent, NavComponent, FooterComponent},
-  data() {
-    return {
-      Popularproducts: [
-        {
-          id: 1,
-          title: 'Iphone 15',
-          storage: '256gb',
-          description:'Apple, 5G compatible, 48MP camera, Splash, water and dust resistant.',
-          color: 'Pink',
-          imageLink: 'src/assets/iphone-15.webp',
-          imageAlt: 'Iphone 15',
-          price: 749,
-          BTW: 749*0.21,
-          Stock: 10
-        },
-        {
-          id: 2,
-          title: 'Iphone 15 Plus',
-          storage: '128gb',
-          description:'Apple, 5G compatible, 48MP camera, Splash, water and dust resistant.',
-          color: 'Black',
-          imageLink: 'src/assets/iphone-15-plus.webp',
-          imageAlt: 'Iphone 15 Plus',
-          price: 999,
-          BTW: 999*0.21,
-          Stock: 12
-        },
-        {
-          id: 3,
-          title: 'Iphone 14',
-          storage: '256gb',
-          description:'Apple, 5G compatible, 48MP camera, Splash, water and dust resistant.',
-          color: 'Yellow',
-          imageLink: 'src/assets/iphone-14.webp',
-          imageAlt: 'Iphone 14',
-          price: 749,
-          BTW: 749*0.21,
-          Stock: 8
-        }
-      ]
-    }
-  }
+  components: {CardComponent, HeaderComponent, NavComponent, FooterComponent,PopularProductsComponent},
 })
 </script>
 
 <template>
   <HeaderComponent/>
-  <div class="gallery">
-  <CardComponent v-for="product in Popularproducts"
-                 v-bind:key="product.id"
-                 v-bind:product="product"
-  />
-  </div>
+ <PopularProductsComponent/>
 </template>
 
 <style scoped>

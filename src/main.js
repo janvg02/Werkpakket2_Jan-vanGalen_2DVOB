1618fa410f31befc9b15daaf7017b27614aa5621
janvg02 <jan.vangalen@student.pxl.be>
2023-12-14 22:50:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ProductsView from "@/views/ProductsView.vue";
 import HomeView from "@/views/HomeView.vue";
 import DetailView from "@/views/DetailView.vue";
 import CartView from "@/views/CartView.vue";
+import CheckoutView from "@/views/CheckoutView.vue";
 import { createPinia } from 'pinia';
 
 
@@ -18,7 +19,8 @@ const router = createRouter({
         {path: '/Login', component: LogIn},
         {path: '/Products', component: ProductsView},
         {path: '/Detail', component: DetailView},
-        {path: '/Cart', component: CartView}
+        {path: '/Cart', component: CartView},
+        {path: '/Checkout', component: CheckoutView}
     ]
 })
 
